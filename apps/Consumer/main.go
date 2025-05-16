e73bd67dc678833c7539d28ce64993bb934a7385
GustaMonteiro <gcvm6070@gmail.com>
2025-05-16 09:16:37 -0300

    Add dockerfiles for two projects

--- apps/Consumer/main.go
+++ apps/Consumer/main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world!")
-
+	fmt.Println("Hello from Consumer")
 	fmt.Println(calculator.Multiply(3, 5))
 }
---
+++ apps/Producer/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"calculator"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello from Producer")
+	fmt.Println(calculator.Multiply(3.5, 2))
+}
